f31dd8328fcfab32fa2fb166807f489c4700b98e
Jakob Ketterl <jketterl@chipxonio.de>
2010-01-13 10:47:38 +0100

    adding ability to fire and parse requests

--- connection.js
+++ connection.js
@@ -12,6 +12,45 @@ exports.class.prototype.processRequest = function(request) {
 	// todo: implement some kind of queueing mechanism here
 	if (this.request) return;
 	this.request = request;
-	if (!(this.request instanceof Memcache.Request)) this.request = new Memcache.Request(request);
+	if (!(this.request instanceof Memcache.Request)) this.request = new Memcache.Request(this.request);
 	this.request.setConnection(this);
+	this.getTcpConnection(function(connection){
+		connection.send(request.command + '\r\n');
+		if (request.data) connection.send(request.data + '\r\n');
+	});
+};
+
+exports.class.prototype.getTcpConnection = function(callback) {
+	if (this.tcpConnection == undefined) {
+		// no connection established? let's start a new one
+		var connection = tcp.createConnection(this.port, this.host);
+		if (callback) connection.addListener('connect', function(){
+			callback(connection);
+		});
+		// ugly closure
+		var method = this;
+		// add event listeners
+		connection.addListener('receive', function(){
+			method.request.parseResponse.apply(method.request, arguments);
+		});
+		connection.addListener('close', function(){
+			method.close.apply(method, arguments);
+		});
+		this.tcpConnection = connection;
+	} else {
+		// connection already established? use this one
+		if (callback) callback(this.tcpConnection);
+	}
+	return this.tcpConnection;
+};
+
+exports.class.prototype.close = function() {
+	if (!this.tcpConnection) return;
+	this.tcpConnection.close();
+	delete this.tcpConnection;
+};
+
+exports.class.prototype.finishRequest = function(request) {
+	if (request != this.request) return;
+	delete this.request;
 };
--- request.js
+++ request.js
@@ -5,3 +5,43 @@ exports.class = function(config){
 exports.class.prototype.setConnection = function(connection){
 	this.connection = connection;
 };
+
+exports.class.prototype.parseResponse = function(data){
+	while (data.length > 0) {
+		if (!this.dataMode) {
+			var index = data.indexOf('\r\n');
+			var response = data.substr(0, index);
+			var data = data.substr(index + 2, data.length - 2);
+			if (response.substring(0, 5) == 'VALUE') {
+				this.dataMode = true;
+				var split = response.split(' ');
+				this.expectedLength = split[3];
+			} else if (response == 'END' || response == 'ERROR' || response == 'STORED') {
+				this.finish(response);
+			} else {
+				// unknown response string
+				require('sys').puts(response);
+			}
+		} else {
+			var chunk = data.substr(0, this.expectedLength);
+			this.expectedLength -= chunk.length;
+			if (this.data) {
+				this.data += chunk;
+			} else {
+				this.data = chunk;
+			}
+			if (chunk.length < data.length) {
+				data = data.substr(chunk.length +2, data.length - chunk.length - 2);
+				delete this.dataMode;
+			} else {
+				data = '';
+			}
+		}
+	}
+};
+
+exports.class.prototype.finish = function(status){
+	this.success = status != 'ERROR';
+	if (this.config.callback) this.config.callback(this);
+	this.connection.finishRequest(this);
+};
