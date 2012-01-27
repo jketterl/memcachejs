c6d459c6c818868aa338157391d8afec64e5378c
Jakob Ketterl <jketterl@chipxonio.de>
2012-01-27 09:05:32 +0100

    integrate request pooling, thx to MiketoString (https://gist.github.com/416704)

--- connection.js
+++ connection.js
@@ -1,23 +1,29 @@
 var Memcache = {
-		Request:require('./request')
+    Request:require('./request')
 };
 var net = require('net');
 var sys = require('sys');
 
 Memcache.Connection = function(host, port){
-	this.host = host;
-	this.port = port;
+    this.host = host;
+    this.port = port;
+
+    this.requestQ = [];
 };
 
 sys.inherits(Memcache.Connection, process.EventEmitter);
 
 Memcache.Connection.prototype.processRequest = function(request) {
-	// todo: implement some kind of queueing mechanism here
-	if (this.request) return;
-	this.emit('status', 'busy');
-	this.request = request;
-	if (!(this.request instanceof Memcache.Request)) this.request = new Memcache.Request(this.request);
-	this.request.setConnection(this);
+    //if already busy, then push on to the request queue
+    if (this.isBusy()) {
+        this.requestQ.push(request);
+        return;
+    }
+
+    this.emit('status', 'busy');
+    this.request = request;
+    if (!(this.request instanceof Memcache.Request)) this.request = new Memcache.Request(this.request);
+    this.request.setConnection(this);
 	this.getTcpConnection(function(connection){
 	    var command = request.command + '\r\n';
 		if (request.data) command += request.data + '\r\n';
@@ -26,56 +32,61 @@ Memcache.Connection.prototype.processRequest = function(request) {
 };
 
 Memcache.Connection.prototype.isBusy = function() {
-	return this.request != undefined;
+    return this.request != undefined;
 };
 
 Memcache.Connection.prototype.getTcpConnection = function(callback) {
-	if (this.tcpConnection == undefined) {
-		// no connection established? let's start a new one
-		var connection = net.createConnection(this.port, this.host);
-		if (callback) connection.addListener('connect', function(){
-			callback(connection);
-		});
-		// ugly closure
-		var method = this;
-		// add event listeners
-		connection.addListener('data', function(){
-			method.request.parseResponse.apply(method.request, arguments);
-		});
-		connection.addListener('close', function(){
-			method.close.apply(method, arguments);
-		});
-		this.tcpConnection = connection;
-	} else {
-		// connection already established? use this one
-		if (callback) callback(this.tcpConnection);
-	}
-	return this.tcpConnection;
+    if (this.tcpConnection == undefined) {
+        // no connection established? let's start a new one
+        var connection = net.createConnection(this.port, this.host);
+        if (callback) connection.addListener('connect', function(){
+            callback(connection);
+        });
+        // ugly closure
+        var method = this;
+        // add event listeners
+        connection.addListener('data', function(){
+            method.request.parseResponse.apply(method.request, arguments);
+        });
+        connection.addListener('close', function(){
+            method.close.apply(method, arguments);
+        });
+        this.tcpConnection = connection;
+    } else {
+        // connection already established? use this one
+        if (callback) callback(this.tcpConnection);
+    }
+    return this.tcpConnection;
 };
 
 Memcache.Connection.prototype.close = function() {
-	if (!this.tcpConnection) return;
-	this.tcpConnection.end();
-	delete this.tcpConnection;
-	if (this.request) {
-		this.request.finish('ERROR');
-	}
-	this.emit('close');
+    if (!this.tcpConnection) return;
+    this.tcpConnection.end();
+    delete this.tcpConnection;
+    if (this.request) {
+        this.request.finish('ERROR');
+    }
+    this.emit('close');
 };
 
 Memcache.Connection.prototype.finishRequest = function(request) {
-	if (request != this.request) return;
-	delete this.request;
-	this.emit('status', 'idle');
+    if (request != this.request) return;
+    delete this.request;
+    this.emit('status', 'idle');
+
+    //if there are requests waiting, then process the next one
+    if(this.requestQ.length > 0) {
+        this.processRequest(this.requestQ.shift());
+    }
 };
 /*
 Memcache.Connection.prototype.setPool = function(pool) {
-	this.pool = pool;
+    this.pool = pool;
 };
 
 Memcache.Connection.prototype.getPool = function() {
-	if (this.pool) return this.pool;
-	return false;
+    if (this.pool) return this.pool;
+    return false;
 };
 */
 
